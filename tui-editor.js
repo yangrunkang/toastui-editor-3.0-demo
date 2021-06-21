83b470b14856191bd8a16d2dd3ad9288c4b84c33
yangrunkang <yangrunkang53@gmail.com>
2021-06-21 23:32:47 +0800

    add some funcions

--- tui-editor.js
+++ tui-editor.js
@@ -2,31 +2,30 @@ const { Editor } = toastui;
 const { chart, codeSyntaxHighlight, colorSyntax, tableMergedCell } = Editor.plugin;
 
 const chartOptions = {
-    minWidth: 100,
-    maxWidth: 600,
-    minHeight: 100,
-    maxHeight: 300
+  minWidth: 100,
+  maxWidth: 600,
+  minHeight: 100,
+  maxHeight: 300
 };
 
-
-
 const editor = new Editor({
-    el: document.querySelector('#editor'),
-    previewStyle: 'vertical',
-    height: '500px',
-    initialValue: allPluginsContent,
-    plugins: [[chart, chartOptions], [codeSyntaxHighlight, { highlighter: Prism }], colorSyntax, tableMergedCell],
-    language: 'zh-CN',
-    events: {
-      load: function(){
-        console.log('编辑器加载完成');
-      },
-      change: function(){
-        console.log('编辑器内容变更');
-      }
+  el: document.querySelector('#editor'),
+  previewStyle: 'vertical',
+  height: '500px',
+  initialValue: allPluginsContent,
+  plugins: [[chart, chartOptions], [codeSyntaxHighlight, { highlighter: Prism }], colorSyntax, tableMergedCell],
+  language: 'zh-CN',
+  events: {
+    load: function () {
+      console.log('编辑器加载完成');
     },
-    hideModeSwitch: true,
+    change: function () {
+      console.log('编辑器内容变更');
+    }
+  },
+  hideModeSwitch: true,
 });
+
 
 // 删除默认监听事件
 editor.removeHook('addImageBlobHook')
@@ -38,3 +37,26 @@ editor.addHook('addImageBlobHook', (blob, callback) => {
   // 这里是回调地址,给编辑器
   callback('https://www.google.com.hk/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png');
 })
+
+
+function getValue(type) {
+  if (type === 'h') {
+    console.log(editor.getHTML());
+  } else {
+    console.log(editor.getMarkdown());
+  }
+}
+
+
+function setValue(type) {
+  if (type === 'h') {
+    editor.setHTML('<h1>设置H1 Html</h1>');
+  } else {
+    editor.setMarkdown('## 设置H2 markdown');
+  }
+}
+
+function reset() {
+  editor.reset();
+}
+
